13521c92da60a35186924f79e46feadcc041e938
dhicks@verisys.com <dhicks@verisys.com>
2019-09-11 13:40:21 -0600

    Adding exclude_paths to DRASTICALLY cleanup the graph (avoiding all 3rd party gem/ActiveRecord calls), include_paths to override exclude_paths for a given class (like mysql2/client), and then the ability to register a lambda to a 'class#method' that will be run to determine the label for the matching node

--- lib/visual_call_graph/graph_manager.rb
+++ lib/visual_call_graph/graph_manager.rb
@@ -3,13 +3,16 @@ require 'graphviz'
 
 class GraphManager
   def initialize(options)
-    @stack   = ["start"]
+    start_label = (options[:start_label]) || "start"
+    start_label.gsub!("\\\"", "'")
+
+    @stack   = [start_label]
     @edges   = []
     @options = options
 
     @g = GraphViz.new(:G, :type => :digraph)
 
-    @g.add_node("start")
+    @g.add_node(start_label)
   end
 
   def add_edges(event)
@@ -25,10 +28,20 @@ class GraphManager
   end
 
   def get_node_name(event)
+    klass_name = event.defined_class.to_s
+    if klass_name.start_with?("#<Class:")
+      # Doh - we've got a Singleton class (probably cuz of a static method)
+      matches = klass_name.match(/#<Class:(\w*).*/)
+      klass_name = matches[1] rescue matches[0]
+    end
+    class_and_method = "#{klass_name}##{event.method_id}"
     if @options[:show_path]
-      "#{event.defined_class}##{event.method_id}\n#{event.path}".freeze
+      "#{class_and_method}\n#{event.path}".freeze
+    elsif @options[class_and_method] # this should be a lambda
+      value = @options[class_and_method].call(event.binding)
+      "#{class_and_method}\n#{value}".freeze
     else
-      "#{event.defined_class}##{event.method_id}".freeze
+      class_and_method.freeze
     end
   end
 
--- lib/visual_call_graph/tracer.rb
+++ lib/visual_call_graph/tracer.rb
@@ -2,7 +2,7 @@
 class Tracer
   def initialize(options)
     @graph = GraphManager.new(options)
-    @tracer = build_tracer
+    @tracer = build_tracer(options)
   end
 
   def enable
@@ -21,14 +21,44 @@ class Tracer
 
   private
 
-  def build_tracer
+  # options:  {
+  #              include_path_contains: [],
+  #              exclude_path_contains: []
+  #           }
+  # include_path_contains take precedence over exclude_path_contains
+  #
+  def build_tracer(options={})
+    include_path_contains = options[:include_path_contains]
+    exclude_path_contains = options[:exclude_path_contains]
     TracePoint.new(:call, :return) { |event|
+      # Avoid tracing myself...
       next if  event.defined_class == self.class
-
+      # Ensure include_path_contains run first and will avoid exclude rules
+      include_matched = false
+      if !include_path_contains.nil?
+        include_path_contains.each do |always_include_paths_containing_this|
+          if event.path.include?(always_include_paths_containing_this)
+            include_matched = true
+            break
+          end
+        end
+      end
+      # Avoid tracing paths including strings specified in exclude_path_contains option 
+      if !include_matched && !exclude_path_contains.nil?
+        skip_tracing = false
+        exclude_path_contains.each do |exlude_paths_containing_this|
+          if event.path.include?(exlude_paths_containing_this)
+            skip_tracing = true
+            break
+          end
+        end
+        next if skip_tracing
+      end
       case event.event
       when :return
         @graph.pop
       when :call
+        puts "TRACING: #{event.path}"
         @graph.add_edges(event)
       end
     }
